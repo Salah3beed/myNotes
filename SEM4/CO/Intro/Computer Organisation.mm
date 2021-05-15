88a613ad192ea94b65872f422428b4bff85d9047
SalahEl-Din Ebeed <salah.aped@gmail.com>
2021-05-15 15:17:31 +0200

    CO_Add: Introduction lectures

---
+++ SEM4/CO/Intro/Computer Organisation.mm
@@ -0,0 +1,254 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Computer Organisation" FOLDED="false" ID="ID_1151641133" CREATED="1619450183078" MODIFIED="1619451009374" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.351">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Serif" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#006666" STYLE="bubble">
+<font NAME="Droid Serif" SIZE="18" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#990099">
+<font NAME="JF Flat" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<font NAME="JF Flat" SIZE="14"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="14" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Basic Computer Orgainstaion" POSITION="right" ID="ID_850135845" CREATED="1619450185524" MODIFIED="1619451024926">
+<edge COLOR="#ff0000"/>
+<node TEXT="Memory" ID="ID_363177838" CREATED="1619451123886" MODIFIED="1619451491146">
+<node TEXT="CPU" ID="ID_506334088" CREATED="1619451127049" MODIFIED="1619451511571">
+<node TEXT="Control Unit" ID="ID_953297807" CREATED="1619451134156" MODIFIED="1619451137204">
+<node TEXT="inputs" ID="ID_1191926116" CREATED="1619451154294" MODIFIED="1619451258297">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_953297807" STARTINCLINATION="77;0;" ENDINCLINATION="77;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Arithmetic Logic Unit" ID="ID_1896939665" CREATED="1619451137561" MODIFIED="1619451514920" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="1.4999999552965235 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1699237132" STARTINCLINATION="46;-23;" ENDINCLINATION="9;-3;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="outputs" ID="ID_1699237132" CREATED="1619451158239" MODIFIED="1619451159661"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Recap" POSITION="right" ID="ID_1029606188" CREATED="1619451616058" MODIFIED="1619451617286">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Multiplexers" ID="ID_1694346586" CREATED="1619451621123" MODIFIED="1619451627423">
+<node TEXT="png_5572118628361382730.png" ID="ID_1721785671" CREATED="1619451629475" MODIFIED="1619451629475">
+<hook URI="Computer%20Organisation_files/png_5572118628361382730.png" SIZE="0.8456569" NAME="ExternalObject"/>
+<node TEXT="png_6682386335460086827.png" ID="ID_941557789" CREATED="1619451643835" MODIFIED="1619451643835">
+<hook URI="Computer%20Organisation_files/png_6682386335460086827.png" SIZE="0.70816106" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Decoders" ID="ID_1112696463" CREATED="1619451670597" MODIFIED="1619451672906">
+<node TEXT="png_3652818817792982192.png" ID="ID_1038074293" CREATED="1619451674023" MODIFIED="1619451674023">
+<hook URI="Computer%20Organisation_files/png_3652818817792982192.png" SIZE="0.8162981" NAME="ExternalObject"/>
+<node TEXT="png_4531525362620663325.png" ID="ID_1113540602" CREATED="1619451689730" MODIFIED="1619451689730">
+<hook URI="Computer%20Organisation_files/png_4531525362620663325.png" SIZE="0.70100963" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Decode from binary to decimal" ID="ID_1214644888" CREATED="1619451751448" MODIFIED="1619451763075"/>
+<node TEXT="Only one output is asserted at a time," ID="ID_1225047545" CREATED="1619451788473" MODIFIED="1619451788970"/>
+</node>
+<node TEXT="D-Flip-Flop" ID="ID_594303748" CREATED="1619451807915" MODIFIED="1619451812747">
+<node TEXT="a memory element that stores one bit of info" ID="ID_637212682" CREATED="1619451813947" MODIFIED="1619451824003">
+<node TEXT="png_2819010567869757552.png" ID="ID_946966976" CREATED="1619451826147" MODIFIED="1619451826147">
+<hook URI="Computer%20Organisation_files/png_2819010567869757552.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Registers" ID="ID_1430161781" CREATED="1619451882678" MODIFIED="1619451885559">
+<node TEXT="multiple flip flops" ID="ID_550733176" CREATED="1619451887902" MODIFIED="1619451897147">
+<node TEXT="png_5519271813589522742.png" ID="ID_1846332736" CREATED="1619451898451" MODIFIED="1619451898451">
+<hook URI="Computer%20Organisation_files/png_5519271813589522742.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="png_6494278704729336555.png" ID="ID_1649090033" CREATED="1619451909062" MODIFIED="1619451909062">
+<hook URI="Computer%20Organisation_files/png_6494278704729336555.png" SIZE="0.71428573" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Tri-State Buffer" ID="ID_374999611" CREATED="1619453244744" MODIFIED="1619453248585">
+<node TEXT="png_4104024350718325249.png" ID="ID_1052548435" CREATED="1619453254562" MODIFIED="1619453254562">
+<hook URI="Computer%20Organisation_files/png_4104024350718325249.png" SIZE="0.39241335" NAME="ExternalObject"/>
+</node>
+<node TEXT="Also called three-state buffer" ID="ID_1844316456" CREATED="1619453262803" MODIFIED="1619453262803"/>
+<node TEXT="It acts like a switch to connect input with output when activated, or open circuit when not." ID="ID_1183092659" CREATED="1619453262803" MODIFIED="1619453262803"/>
+<node ID="ID_1536698976" CREATED="1619453262804" MODIFIED="1619453280095"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The open circuit state is called <b>High impedance </b>and does not have any logic significance.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Internal Hardware Organisation of a computer system" POSITION="left" ID="ID_1800736" CREATED="1619452174292" MODIFIED="1619452190897">
+<edge COLOR="#00007c"/>
+<node TEXT="is defined as" ID="ID_1351132948" CREATED="1619452191900" MODIFIED="1619452204385">
+<node TEXT="the set of registers it contains" ID="ID_276784208" CREATED="1619452205417" MODIFIED="1619452212728"/>
+<node TEXT="sequence of micro operations performed" ID="ID_1743386820" CREATED="1619452212908" MODIFIED="1619452228534"/>
+<node TEXT="the control that makes these operations happen" ID="ID_1594754148" CREATED="1619452230636" MODIFIED="1619452243996"/>
+</node>
+</node>
+<node TEXT="Register Transfer Language RTL" POSITION="left" ID="ID_1959337005" CREATED="1619452397586" MODIFIED="1619452413017">
+<edge COLOR="#7c7c00"/>
+<node TEXT="R2 &lt;- R1" ID="ID_1296177315" CREATED="1619452465045" MODIFIED="1619452470219">
+<node TEXT="this means" ID="ID_1138539531" CREATED="1619452472225" MODIFIED="1619452475608">
+<node TEXT="the content of R1 will be copied to R2" ID="ID_78136473" CREATED="1619452486590" MODIFIED="1619452494221"/>
+</node>
+</node>
+<node TEXT="P: R2&lt;-R1" ID="ID_1882245514" CREATED="1619452682647" MODIFIED="1619452688360">
+<node TEXT="if P(true) transfer the data in R1 to R2" ID="ID_232875868" CREATED="1619452692025" MODIFIED="1619452704670"/>
+</node>
+</node>
+<node TEXT="How to represent a register" POSITION="left" ID="ID_913014740" CREATED="1619452516680" MODIFIED="1619452521489">
+<edge COLOR="#0000ff"/>
+<node TEXT="using only name" ID="ID_1310469183" CREATED="1619452534013" MODIFIED="1619452538183">
+<node TEXT="png_5506723550403120726.png" ID="ID_1778496769" CREATED="1619452570190" MODIFIED="1619452570190">
+<hook URI="Computer%20Organisation_files/png_5506723550403120726.png" SIZE="0.6367962" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="individual bits" ID="ID_1544066150" CREATED="1619452538538" MODIFIED="1619452542774">
+<node TEXT="png_1337075919639709778.png" ID="ID_339680083" CREATED="1619452586420" MODIFIED="1619452586420">
+<hook URI="Computer%20Organisation_files/png_1337075919639709778.png" SIZE="0.66117495" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="MSB and LSB" ID="ID_1029965575" CREATED="1619452543150" MODIFIED="1619452547135">
+<node TEXT="png_6910453949603693014.png" ID="ID_39544167" CREATED="1619452596237" MODIFIED="1619452596237">
+<hook URI="Computer%20Organisation_files/png_6910453949603693014.png" SIZE="0.5271319" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="High order Byte and Low order Byte" ID="ID_1101846982" CREATED="1619452553097" MODIFIED="1619452562013">
+<node TEXT="png_4507439072032792544.png" ID="ID_501082211" CREATED="1619452655331" MODIFIED="1619452655331">
+<hook URI="Computer%20Organisation_files/png_4507439072032792544.png" SIZE="0.5782444" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Using a BUS" POSITION="left" ID="ID_664544555" CREATED="1619452838557" MODIFIED="1619452841495">
+<edge COLOR="#00ff00"/>
+<node TEXT="we use a bus to connect all registers to each other without the huge amount of cables needed" ID="ID_2622331" CREATED="1619452842369" MODIFIED="1619452962269">
+<node TEXT="common bus" ID="ID_421730786" CREATED="1619452868229" MODIFIED="1619452872513">
+<node TEXT="png_121655205749627850.png" ID="ID_969639178" CREATED="1619452912786" MODIFIED="1619452912786">
+<hook URI="Computer%20Organisation_files/png_121655205749627850.png" SIZE="0.40733197" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Each register can either read from the bus or write to it." ID="ID_207090567" CREATED="1619452934206" MODIFIED="1619452934206">
+<node TEXT="What if two register want to write on the bus ?" ID="ID_1514776065" CREATED="1619452976749" MODIFIED="1619452996159"/>
+</node>
+<node TEXT="A common bus is a set of common lines, shared by all registers." ID="ID_1364071513" CREATED="1619452934206" MODIFIED="1619452934206"/>
+</node>
+<node TEXT="BUS Control" ID="ID_1012756065" CREATED="1619452997307" MODIFIED="1619453001855">
+<node TEXT="there must be some control on who reads or who writes on the bus every clock cycle." ID="ID_202173917" CREATED="1619453013479" MODIFIED="1619453014117">
+<node TEXT="Two registers must not put data on the bus at the same time." ID="ID_223593253" CREATED="1619453030220" MODIFIED="1619453030220"/>
+<node TEXT="However, two registers can read (be loaded) data from the bus at the same time. There’s no conflict here." ID="ID_1568402449" CREATED="1619453030220" MODIFIED="1619453030220"/>
+</node>
+</node>
+<node TEXT="png_1386719069920480791.png" ID="ID_62380876" CREATED="1619453088383" MODIFIED="1619453088383">
+<hook URI="Computer%20Organisation_files/png_1386719069920480791.png" SIZE="0.5076142" NAME="ExternalObject"/>
+<node TEXT="for S1S0 = 00" ID="ID_487081367" CREATED="1619453161936" MODIFIED="1619453166982">
+<node TEXT="output is Register A" ID="ID_299013137" CREATED="1619453180585" MODIFIED="1619453190377">
+<node TEXT="A3&#xa;A2&#xa;A1&#xa;A0" ID="ID_488092994" CREATED="1619453191252" MODIFIED="1619453198507"/>
+</node>
+</node>
+</node>
+<node TEXT="We can make a common bus using tr-state buffers" ID="ID_100086761" CREATED="1619453285855" MODIFIED="1619453297898">
+<node TEXT="png_1267491627265843999.png" ID="ID_1460720774" CREATED="1619453381577" MODIFIED="1619453381577">
+<hook URI="Computer%20Organisation_files/png_1267491627265843999.png" SIZE="0.30663577" NAME="ExternalObject"/>
+<node TEXT="png_3527358102384483793.png" ID="ID_1124374885" CREATED="1619453406634" MODIFIED="1619453406634">
+<hook URI="Computer%20Organisation_files/png_3527358102384483793.png" SIZE="0.38535646" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Note that only one decoder is used" ID="ID_1128074085" CREATED="1619453417387" MODIFIED="1619453425826">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="Micro Operations (Miroops)" POSITION="right" ID="ID_274384734" CREATED="1619451980756" MODIFIED="1619451989917">
+<edge COLOR="#7c0000"/>
+<node TEXT="Data can be stored in Registers and performed upon using micro ops" ID="ID_788861292" CREATED="1619452034265" MODIFIED="1619452053006"/>
+<node TEXT="We can perform operations on data from registers and then the output can be stored into another register" ID="ID_450908395" CREATED="1619452103125" MODIFIED="1619452133608">
+<node TEXT="png_3307989091364766547.png" ID="ID_1464091429" CREATED="1619452135011" MODIFIED="1619452135011">
+<hook URI="Computer%20Organisation_files/png_3307989091364766547.png" SIZE="0.5522978" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Note" ID="ID_1533627037" CREATED="1619452277307" MODIFIED="1619452357397">
+<node TEXT="A program written in any copmuter language is usually converted into a sequence of instructions" ID="ID_108250908" CREATED="1619452282729" MODIFIED="1619452311094"/>
+<node TEXT="each instruction consists of one or more micro ops" ID="ID_977864159" CREATED="1619452311441" MODIFIED="1619452329318"/>
+<node TEXT="all this happens in machine language (like Assembly)" ID="ID_969153740" CREATED="1619452333808" MODIFIED="1619452347539"/>
+</node>
+</node>
+</node>
+</map>
