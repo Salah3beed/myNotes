d6d0ca51b72f5d3c378bbb058f1eb27cbd5fef28
SalahEl-Din Ebeed <salah.aped@gmail.com>
2021-05-18 02:14:46 +0200

    Strength_Rearrange: Lecture files

---
+++ SEM4/stress1/Intro/Intro to Stress.mm
@@ -0,0 +1,227 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Intro to Stress" FOLDED="false" ID="ID_273747114" CREATED="1618247532249" MODIFIED="1618267718842"><hook NAME="MapStyle" zoom="0.565">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<font SIZE="15"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="HEADINGS"/>
+<node TEXT="Strength of Materials" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_6148869" CREATED="1618247689482" MODIFIED="1618268070249">
+<node TEXT="The branch of applied mechanics that deals with the behavior of elastic bodies subjected to various types of loading" ID="ID_1437256716" CREATED="1618247708076" MODIFIED="1618247745518">
+<node TEXT="Bodies are components of a " ID="ID_1310308140" CREATED="1618266244737" MODIFIED="1618266262008">
+<node TEXT="machine " ID="ID_646147776" CREATED="1618266263089" MODIFIED="1618266264718"/>
+<node TEXT="or a structure" ID="ID_1477329584" CREATED="1618266265196" MODIFIED="1618266268170"/>
+</node>
+</node>
+</node>
+<node TEXT="Types of loading " POSITION="right" ID="ID_699741385" CREATED="1618266274060" MODIFIED="1618267264627">
+<font SIZE="11"/>
+<node TEXT="Axial" ID="ID_1853389208" CREATED="1618266278688" MODIFIED="1618266956551">
+<node TEXT="in Bars" ID="ID_809582352" CREATED="1618266292730" MODIFIED="1618267006447">
+<font NAME="SansSerif" SIZE="13" BOLD="true"/>
+<node ID="ID_1039115922" CREATED="1618266353238" MODIFIED="1618268072478"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Intro to Stress_6146854079621044596.jpeg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Torsion" ID="ID_552019687" CREATED="1618266280817" MODIFIED="1618266956554">
+<node TEXT="in Shafts" ID="ID_1046394040" CREATED="1618266296254" MODIFIED="1618267006446">
+<font NAME="SansSerif" SIZE="13" BOLD="true"/>
+<node ID="ID_274622761" CREATED="1618266364240" MODIFIED="1618266364240"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Intro to Stress_5909927011673891766.jpeg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Bending " ID="ID_1240424456" CREATED="1618266285911" MODIFIED="1618266956545">
+<node TEXT="in Beams" ID="ID_149363544" CREATED="1618266303429" MODIFIED="1618267006438">
+<font NAME="SansSerif" SIZE="13" BOLD="true"/>
+<node ID="ID_1546633459" CREATED="1618266372199" MODIFIED="1618266372199"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Intro to Stress_1584071176204987843.jpeg" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Design Vs Analysis" POSITION="right" ID="ID_155729726" CREATED="1618266395277" MODIFIED="1618267265554">
+<font SIZE="11"/>
+<node TEXT="Design" ID="ID_1657153331" CREATED="1618266408107" MODIFIED="1618266409738">
+<node TEXT="to take a load and design/create a structure that can hold that load, so the structure does not exist yet" ID="ID_1417884439" CREATED="1618266424136" MODIFIED="1618266457073"/>
+</node>
+<node TEXT="Analyze" ID="ID_256549745" CREATED="1618266458893" MODIFIED="1618266464706">
+<node TEXT="to check if the dimensions and the material that already exist is able to handle that load ? " FOLDED="true" ID="ID_379865929" CREATED="1618266467487" MODIFIED="1618266493377">
+<node ID="ID_472005544" CREATED="1618266494919" MODIFIED="1618267000648"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      if not we get the <b>Maximum</b>&#160;load we can bear
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="13"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Stress" POSITION="right" ID="ID_1086254007" CREATED="1618267028794" MODIFIED="1618267266396">
+<font SIZE="11"/>
+<node TEXT="\latex $\sigma = \lim_{\Delta A \to 0} \frac{\Delta F}{\Delta A}= N/m^2$" ID="ID_1979625841" CREATED="1618267125373" MODIFIED="1618269776694">
+<font SIZE="24"/>
+</node>
+<node TEXT="\latex $\sigma_{ave}  = \frac{P}{A}$" ID="ID_433333580" CREATED="1618269691616" MODIFIED="1618269780829">
+<font SIZE="23"/>
+</node>
+<node ID="ID_17047467" CREATED="1618269791043" MODIFIED="1618269825973"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      normal stress at a point may not be equal to <b>average </b>stress but the <b>resultant </b>of the stress ditribution
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="\latex $P = \sigma_{ave} * A = \int_A \sigma dA$" ID="ID_1871777268" CREATED="1618269827604" MODIFIED="1618269872313">
+<font SIZE="19"/>
+</node>
+</node>
+<node TEXT="Stress types" POSITION="left" ID="ID_1501145193" CREATED="1618267290871" MODIFIED="1618268078609">
+<node TEXT="Normal" ID="ID_1858536086" CREATED="1618267302135" MODIFIED="1618267304035">
+<node TEXT="Force normal to cross section" ID="ID_1402226965" CREATED="1618267321257" MODIFIED="1618267350799">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Shear" ID="ID_1327426141" CREATED="1618267305192" MODIFIED="1618267306553">
+<node TEXT="Force parallel to cross section" ID="ID_496040206" CREATED="1618267333531" MODIFIED="1618267348906">
+<font SIZE="12"/>
+</node>
+<node TEXT="Single" ID="ID_1353402408" CREATED="1618267311142" MODIFIED="1618267315849"/>
+<node TEXT="Double" ID="ID_961220677" CREATED="1618267316130" MODIFIED="1618267317563"/>
+</node>
+<node TEXT="Bearing" ID="ID_281583829" CREATED="1618267307228" MODIFIED="1618267309195"/>
+</node>
+<node TEXT="Normal Load" POSITION="left" ID="ID_886286983" CREATED="1618267368711" MODIFIED="1618267372895">
+<node TEXT="Centric" ID="ID_981978056" CREATED="1618267379496" MODIFIED="1618267382390">
+<node TEXT="line of action of the resultant internal forces passes through the centroid" ID="ID_1777972313" CREATED="1618267390708" MODIFIED="1618267421718"/>
+<node TEXT="Uniform distribution of stress is possbile in this case, where the concetrated loads on the end sections of the two force memers are applied at the section centroids" ID="ID_1952949314" CREATED="1618267514067" MODIFIED="1618267554599"/>
+<node TEXT="centric" ID="ID_754037816" CREATED="1618268202833" MODIFIED="1618268217005">
+<hook URI="Intro%20to%20Stress_files/png_3251418021548988220.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Eccentric" ID="ID_521385499" CREATED="1618267382961" MODIFIED="1618267385932">
+<node TEXT="when it doesn&apos;t" ID="ID_790956132" CREATED="1618267442802" MODIFIED="1618267447246"/>
+<node TEXT="so the stress distribution in a section yields an axial force and a moment" ID="ID_1201571108" CREATED="1618267450762" MODIFIED="1618267496183"/>
+<node TEXT="eccentrically loaded members&apos; stress cannot be uniform or symmetric" ID="ID_826991089" CREATED="1618267559742" MODIFIED="1618267587583"/>
+<node TEXT="eccentric.png" ID="ID_865075594" CREATED="1618268231437" MODIFIED="1618268231437">
+<hook URI="Intro%20to%20Stress_files/eccentric.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Normal Force Diagram" POSITION="left" ID="ID_568195484" CREATED="1618267593248" MODIFIED="1618267598963">
+<node TEXT="Vertical" ID="ID_1932427016" CREATED="1618267602252" MODIFIED="1618267604148">
+<node TEXT="png_6370130947338596632.png" ID="ID_1661018913" CREATED="1618268338324" MODIFIED="1618268338324">
+<hook URI="Intro%20to%20Stress_files/png_6370130947338596632.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="png_3137206622015574129.png" ID="ID_1424410194" CREATED="1618269235381" MODIFIED="1618269235381">
+<hook URI="Intro%20to%20Stress_files/png_3137206622015574129.png" SIZE="0.46404597" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="Horizontal" ID="ID_445824185" CREATED="1618267604747" MODIFIED="1618267606923">
+<node TEXT="png_4464046463029598090.png" ID="ID_134606225" CREATED="1618268273315" MODIFIED="1618268273315">
+<hook URI="Intro%20to%20Stress_files/png_4464046463029598090.png" SIZE="0.8878598" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+</map>
